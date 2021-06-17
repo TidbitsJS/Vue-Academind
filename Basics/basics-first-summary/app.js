17921096cac6ee3522f5419676e6462fabd05681
Tidbits JS <tidbitsjs@gmail.com>
2021-06-17 23:37:41 +0530

    methods & raw html

--- Basics/basics-first-summary/app.js
+++ Basics/basics-first-summary/app.js
@@ -2,8 +2,21 @@ const app = Vue.createApp({
   data: function () {
     return {
       courseGoal: "Finish the course and learn Vue!",
+      courseGoalA: "Code in Vue!",
+      courseGoalB: "Master Vue & build projects",
+      htmlGoal: "<h1>Keep Coding.</h1>",
       vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal: function () {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
