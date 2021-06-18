7d07e418339a94223aa086b098da374e0e063e4a
Tidbits JS <tidbitsjs@gmail.com>
2021-06-18 18:33:30 +0530

    watchers & computed properties

--- Basics/basic-counter/app.js
+++ Basics/basic-counter/app.js
@@ -3,9 +3,26 @@ const app = Vue.createApp({
     return {
       counter: 100,
       name: "",
+      lastName: "",
       coder: "",
       nickname: "",
+      me: "",
     };
+  },
+  watch: {
+    nickname: function (value) {
+      // this.me = this.nickanme + " " + "is awesome!"
+      this.me = value + " " + "is awesome!";
+    },
+  },
+  computed: {
+    fullname() {
+      console.log("Running again....");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
   },
   methods: {
     increment: function () {
@@ -26,6 +43,15 @@ const app = Vue.createApp({
     setCoder: function (event, tag) {
       this.coder = event.target.value + " " + tag;
     },
+    // Not good to use directly
+    outputFullName() {
+      // Vue will reexecute if anything changes including counter & other methods.
+      console.log("Running again....");
+      if (this.coder === "") {
+        return "";
+      }
+      return event.target.value + " " + "is the best coder";
+    },
     setNickname: function () {
       this.nickname = event.target.value;
       event.target.value = "";
@@ -39,6 +65,7 @@ const app = Vue.createApp({
     },
     resetInput: function () {
       this.name = "";
+      this.lastName = "";
     },
   },
 });
