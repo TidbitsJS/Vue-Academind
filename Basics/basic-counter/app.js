e55833c0e4713656388af57e2189435aab97b63a
Tidbits JS <tidbitsjs@gmail.com>
2021-06-18 17:23:01 +0530

    vue directives

--- Basics/basic-counter/app.js
+++ Basics/basic-counter/app.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
   data: function () {
     return {
-      counter: 0,
+      counter: 100,
+      name: "",
+      coder: "",
+      nickname: "",
     };
   },
   methods: {
@@ -10,6 +13,32 @@ const app = Vue.createApp({
     },
     decrement: function () {
       this.counter--;
+    },
+    addFive: function (num) {
+      this.counter += num;
+    },
+    reduceFive: function (num) {
+      this.counter -= num;
+    },
+    setName: function (event) {
+      this.name = event.target.value;
+    },
+    setCoder: function (event, tag) {
+      this.coder = event.target.value + " " + tag;
+    },
+    setNickname: function () {
+      this.nickname = event.target.value;
+      event.target.value = "";
+    },
+    submitForm() {
+      event.preventDefault();
+      alert("Submitted!");
+    },
+    submitNextForm() {
+      alert("Form submitted with vue functionality!");
+    },
+    resetInput: function () {
+      this.name = "";
     },
   },
 });
