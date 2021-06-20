83e50029fc386d6d912011da3b04904887313d30
Tidbits JS <tidbitsjs@gmail.com>
2021-06-20 14:38:05 +0530

    heal surrender & game over

--- Project Monster/app.js
+++ Project Monster/app.js
@@ -8,20 +8,53 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
     },
   },
+  watch: {
+    playerHealth: function (value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        // Player lost
+        this.winner = "Monster";
+      }
+    },
+    monsterHealth: function (value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // A draw
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        // Monster Lost
+        this.winner = "Player";
+      }
+    },
+  },
   methods: {
+    startGame: function () {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -37,6 +70,20 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+
+      this.attackPlayer();
+    },
+    surrender() {
+      this.winner = "Monster";
     },
   },
 });
