32f96b6fcb65ea0f04edf14d232ff2462d1dea44
Tidbits JS <tidbitsjs@gmail.com>
2021-06-23 18:01:33 +0530

    local components & scoped styles

---
+++ Vue CLI/course-goals App/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- <the-header></the-header> -->
+    <TheHeader />
+    <!-- <badge-list></badge-list> -->
+    <BadgeList />
+    <!-- <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info> -->
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    />
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  components: {
+    // "the-header": TheHeader,
+    // TheHeader: TheHeader,
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
