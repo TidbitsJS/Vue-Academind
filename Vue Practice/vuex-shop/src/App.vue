932b0f4e6b6787463ea3f19a8e8984b9b789b51e
Tidbits JS <tidbitsjs@gmail.com>
2021-06-28 22:19:53 +0530

    vuex challenge

---
+++ Vue Practice/vuex-shop/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/nav/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
